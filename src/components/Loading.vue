fd057ef1429ca746e874e3865bb747b55c61701b
bittoMazza <robertomazza99@gmail.com>
2022-07-19 17:02:24 +0200

    add style on loading screen

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="loading-container text-white">
-    ATTENDERE IL CARICAMENTO
+    <span class="loading-text">ATTENDERE IL CARICAMENTO </span> 
+    <div class="spinner-border" role="status">
+    </div>
   </div>
 </template>
 
@@ -11,4 +13,13 @@ export default {
 </script>
 
 <style lang="scss">
+    .loading-container{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        .loading-text{
+            font-size: 40px;
+        }
+    }
 </style>
