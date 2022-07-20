2ddbecac4d754139d0b14b48863e747ee394edb9
bittoMazza <robertomazza99@gmail.com>
2022-07-20 15:14:48 +0200

    add style on select

--- src/components/SelectedGenre.vue
+++ src/components/SelectedGenre.vue
@@ -1,7 +1,12 @@
 <template>
-  <select v-model="selected" name="selectType" @change="$emit('selectGenre',selected)" id="">
-    <option v-for="(card,index) in albums" :key="index" :value='card'> {{ card }}</option>
-  </select>
+<div class="my-4">
+    <select class="W-100 rounded" v-model="selected" name="selectType" @change="$emit('selectGenre',selected)" id="">
+        <option value="" disabled selected>SELEZIONA CHE GENERE VUOI ASCOLTARE !!</option>
+        <option value="" class="text-danger">RESET RICERCA PER GENERE</option>
+        <option v-for="(card,index) in albums" :key="index" :value='card'> {{ card }}</option>
+    </select>
+</div>
+ 
 </template>
 
 <script>
@@ -17,6 +22,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+ select{
+    margin: 30px 0;
+    padding: 10px;
+    text-align: center;
+    
+ }
 </style>
